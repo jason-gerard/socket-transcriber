d379e25e142d12f4bf922ca880ebae4c3d14253b
Jason Gerard <jasongerard321@gmail.com>
2022-01-15 11:50:11 -0500

    added web socket component to server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const path = require('path');
+const http = require("http");
+const websocket = require("ws");
 
 const app = express();
 app.use(express.json());
@@ -9,10 +11,12 @@ app.use(cors());
 
 app.get('/token', async (req, res) => {
     try {
-        const response = await axios.post('https://api.assemblyai.com/v2/realtime/token', // use account token to get a temp user token
-            { expires_in: 3600 }, // can set a TTL timer in seconds.
-            { headers: { authorization: 'fc387af2eb7b4ad38b6c4067885d43db' } }); // AssemblyAI API Key goes here
-        const { data } = response;
+        const { data } = await axios.post(
+            'https://api.assemblyai.com/v2/realtime/token',
+            { expires_in: 3600 },
+            { headers: { authorization: 'fc387af2eb7b4ad38b6c4067885d43db' } }
+        );
+
         res.json(data);
     } catch (error) {
         console.log(error)
@@ -36,6 +40,21 @@ app.get('/room-select', async (req, res) => {
 app.set('port', 5000);
 app.use(express.static('public'));
 
-const server = app.listen(app.get('port'), () => {
-    console.log(`Server is running on port ${server.address().port}`);
+const server = http.createServer(app);
+
+const wss = new websocket.WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (m) => {
+        wss.clients
+            .forEach(client => {
+                if (client !== ws) {
+                    client.send(`broadcast message -> ${m}`);
+                }
+            });
+    });
+});
+
+server.listen(5000, () => {
+    console.log(`Server started on port ${server.address().port}`);
 });
